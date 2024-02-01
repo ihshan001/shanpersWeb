c15e66e6f3c88f44b7c1f7190c8ab38900e5086e
ihshan001 <141259687+ihshan001@users.noreply.github.com>
2024-02-01 07:44:16 +0530

    Add files via upload
    Personal Web Added

---
+++ anim.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+    const body = document.body;
+    const modeToggle = document.getElementById('modeToggle');
+
+    body.classList.toggle('dark-mode');
+    
+    if (body.classList.contains('dark-mode')) {
+        modeToggle.innerHTML = 'Light';
+    } else {
+        modeToggle.innerHTML = 'Dark';
+    }
+}
+
+function downloadCV() {
+    const cvURL = "shancv.pdf"; //replacin the cv location
+    const aElement = document.createElement('a');
+    aElement.setAttribute('href', cvURL);
+    aElement.setAttribute('download', 'IhshanCV.pdf'); // givin a name for cv
+    aElement.click();
+  }
+
+  // Add confirmation message after submitting the contact form
+  const contactForm = document.getElementById('contact-form');
+  contactForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+      alert('Thank you for reaching out! I will get back to you soon.');
+      });
